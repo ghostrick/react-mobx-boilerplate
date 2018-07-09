0fe50b532a56db0377dab59b0a5e49a1fa6b4b78
Ghostrick <ghostrick.kinako@gmail.com>
2018-07-10 07:36:21 +0900

    ⛩ Remove React.Fragment

--- src/components/Router.js
+++ src/components/Router.js
@@ -7,13 +7,11 @@ import Counter from './pages/Counter'
 
 export default () => {
   return (
-    <React.Fragment>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/count" exact component={Counter} />
-        </Switch>
-      </BrowserRouter>
-    </React.Fragment>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/count" exact component={Counter} />
+      </Switch>
+    </BrowserRouter>
   )
 }
